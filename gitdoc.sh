a6b522cf4eaa45aa2dfd93d37613da00d128bfff
HMW-Alexander <alexanderhmw@gmail.com>
2017-01-18 23:30:01 -0500

    auto-update

--- gitdoc.sh
+++ gitdoc.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 for d in */;
 do
-  cd "$COURSE"
+  cd $d
+  echo $PWD
   git add -A
   git commit -a -m "auto-update"
   cd ..
 done
+git add -A
+git commit -a -m "auto-update"
 git push --recurse-submodules=on-demand
