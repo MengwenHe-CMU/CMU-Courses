66634f6072988af882bb00f757a5daf499616cb8
HMW-Alexander <alexanderhmw@gmail.com>
2017-01-19 00:28:30 -0500

    auto-update

--- pushdoc.sh
+++ pushdoc.sh
@@ -5,8 +5,9 @@ do
   echo $PWD
   git add -A
   git commit -a -m "auto-update"
+  git push
   cd ..
 done
 git add -A
 git commit -a -m "auto-update"
-git push --recurse-submodules=on-demand
+git push
